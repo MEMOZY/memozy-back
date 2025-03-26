962f7139684d658d912c21853e2b7c0d769d16fb
Minkyu Shin <digi1k2001@gmail.com>
2025-03-26 12:21:55 +0900

    Initial commit: Spring Boot 프로젝트 세팅

---
+++ src/main/java/com/momozy/memozy_back/MemozyBackApplication.java
@@ -0,0 +1,13 @@
+package com.momozy.memozy_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemozyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemozyBackApplication.class, args);
+	}
+
+}
