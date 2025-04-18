7e6926e03ee91fb386ccf33db6acc6022e042b57
Minkyu Shin <digi1k2001@gmail.com>
2025-04-18 14:10:11 +0900

    feat: swagger authorize 버튼 추가

--- src/main/java/com/memozy/memozy_back/global/config/SwaggerConfig.java
+++ src/main/java/com/memozy/memozy_back/global/config/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package com.memozy.memozy_back.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
@@ -11,6 +15,14 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name(HttpHeaders.AUTHORIZATION)))
                 .info(new Info()
                         .title("MEMOZY API")
                         .description("기록을 쉽게! MEMOZY API 명세서")
