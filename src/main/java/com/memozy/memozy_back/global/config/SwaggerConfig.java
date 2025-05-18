13dc3f3c6bb99afc01a5795fea3e8d87fa9d5ed1
Minkyu Shin <digi1k2001@gmail.com>
2025-05-19 03:19:27 +0900

    fix: swagger request url to https

--- src/main/java/com/memozy/memozy_back/global/config/SwaggerConfig.java
+++ src/main/java/com/memozy/memozy_back/global/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -12,9 +14,13 @@ import org.springframework.http.HttpHeaders;
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.server-url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url(serverUrl))
                 .addSecurityItem(new SecurityRequirement().addList("JWT"))
                 .components(new Components()
                         .addSecuritySchemes("JWT", new SecurityScheme()
@@ -26,7 +32,6 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("MEMOZY API")
                         .description("기록을 쉽게! MEMOZY API 명세서")
-                        .version("v1.0.0")
-                );
+                        .version("v1.0.0"));
     }
 }
