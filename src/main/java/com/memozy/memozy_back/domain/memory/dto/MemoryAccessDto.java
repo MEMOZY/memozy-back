204b3a0ae51e57959e8e38d6c7a67fe7ae580628
Minkyu Shin <digi1k2001@gmail.com>
2025-09-30 20:44:51 +0900

    #165 feat: 권한 정보에 유저 닉네임도 포함

--- src/main/java/com/memozy/memozy_back/domain/memory/dto/MemoryAccessDto.java
+++ src/main/java/com/memozy/memozy_back/domain/memory/dto/MemoryAccessDto.java
@@ -5,10 +5,11 @@ import com.memozy.memozy_back.domain.memory.domain.MemoryAccess;
 
 public record MemoryAccessDto(
         Long userId,
+        String username,
         PermissionLevel permissionLevel
 ) {
     public static MemoryAccessDto of(MemoryAccess memoryAccess) {
 
-        return new MemoryAccessDto(memoryAccess.getUser().getId(), memoryAccess.getPermissionLevel());
+        return new MemoryAccessDto(memoryAccess.getUser().getId(), memoryAccess.getUser().getUsername(), memoryAccess.getPermissionLevel());
     }
 }
